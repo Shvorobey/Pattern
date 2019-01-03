7553386e995a503739f4a86744ac2a13ce13ab5c
Shvorobey <a.shvorobey@gmail.com>
2019-01-03 23:19:19 +0200

    Начало реализации фабричного метода

---
+++ Factory-method/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Torvald
+ * Date: 03.01.2019
+ * Time: 23:14
+ */
+
+function htmlToBBCode($html)
+{
+    $search = ['<b>', '</b>', '<i>', '</i>', '<u>', '</u>'];
+    $replace = ['[b]', '[/b]', '[i]', '[/i]', '[u]', '[/u]'];
+
+    return str_replace($search, $replace, $html);
+}
+
+function htmlToMarkdown($html)
+{
+    $search = ['<b>', '</b>', '<i>', '</i>', '<u>', '</u>'];
+    $replace = ['**', '**', '__', '__', '+', '+'];
+
+    return str_replace($search, $replace, $html);
+}
+
+$html = '<b>Полужирный</b>. <i>Курсив</i>.';
+
+echo htmlToBBCode($html) . '</br></br>';
+echo htmlToMarkdown($html) . '</br></br>';
